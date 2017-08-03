2b47a75a7d1a05dfef3575fd5bbb752f8c82f593
root <root@ip-172-31-35-195.ec2.internal>
2017-08-03 10:53:28 -0400

    Running ant

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,17 +1,17 @@
-public class Rectangle{
-	public int length;
-	public int width;
+public class Rectangle {
+  public int length;
+  public int width;
 
-	public Rectangle(int  length, int width){
-		this.length = length;
-		this.width = width;
-	}
+  public Rectangle(int length, int width) {
+    this.length = length;
+    this.width = width;
+  }
 
-	public int getArea(){
-		retrun length * witdth;
-	}
+  public int getArea() {
+    return length * width;
+  }
 
-	public int getPermiter(){
-		return 2 * ( length + width);
-	}
+  public int getPerimeter() {
+    return 2 * (length + width);
+  }
 }
